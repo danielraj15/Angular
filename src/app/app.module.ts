be4be1e68bae3140d5a296c973cd0ee61e81d087
unknown <15raj.dani@gmail.com>
2018-08-25 20:59:57 +0530

    Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FieldsComponent } from './fields/fields.component';
+import { WhatsnewComponent } from './whatsnew/whatsnew.component';
+import {RouterModule,Routes} from '@angular/router';
+
+const approutes:Routes=[
+{path: 'home',component:HomeComponent},
+{path: 'fields',component:FieldsComponent},
+{path: 'whatsnew',component:WhatsnewComponent},
+{path: '',redirectTo:'/home',pathMatch:'full'},
+{path: '**',redirectTo:'/home',pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FieldsComponent,
+    WhatsnewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
